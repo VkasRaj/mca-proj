0c99e9c154f406a06c342a026f764e77f7559988
vkasraj <vikasraj1911@gmail.com>
2018-11-08 13:58:31 +0530

    Updates

---
+++ precache-manifest.126a21814bdf9f08115440f6228182b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afda76f95c7b61dde39c",
+    "url": "/static/js/main.afda76f9.chunk.js"
+  },
+  {
+    "revision": "07fc52579cd71bceccdd",
+    "url": "/static/js/1.07fc5257.chunk.js"
+  },
+  {
+    "revision": "d1378498333843bd085b36e1b3a802fb",
+    "url": "/index.html"
+  }
+];
